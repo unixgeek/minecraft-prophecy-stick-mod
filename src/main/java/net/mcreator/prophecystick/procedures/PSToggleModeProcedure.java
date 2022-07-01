862bb45e6411a256ca3a5dd7f1ee35e01600c2ff
unixgeek <gunter.wambaugh@gmail.com>
2022-07-01 11:31:29 -0500

    Updated github build to jdk 17. Fixed toggle procedure.

--- src/main/java/net/mcreator/prophecystick/procedures/PSToggleModeProcedure.java
+++ src/main/java/net/mcreator/prophecystick/procedures/PSToggleModeProcedure.java
@@ -7,59 +7,38 @@ import net.minecraft.network.chat.TextComponent;
 import net.mcreator.prophecystick.network.ProphecyStickModVariables;
 
 public class PSToggleModeProcedure {
-	public static void execute(Entity entity) {
-		if (entity == null)
-			return;
-		{
+    public static void execute(Entity entity) {
+        if (entity == null)
+            return;
+        {
+            double mode = entity.getCapability(ProphecyStickModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+                    .orElse(new ProphecyStickModVariables.PlayerVariables()).prophecy_stick_mode;
 
-			double mode = entity.getCapability(ProphecyStickModVariables.PLAYER_VARIABLES_CAPABILITY, null)
-				.orElse(new ProphecyStickModVariables.PlayerVariables())).prophecy_stick_mode);
-
-			mode = (int) mode << 1;
+            mode = (int) mode << 1;
             if (mode > 64) {
                 mode = 1;
             }
 
-			entity.getCapability(ProphecyStickModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-				capability.prophecy_stick_mode = mode;
-				capability.syncPlayerVariables(entity);
-			});
+            double finalMode = mode;
+            entity.getCapability(ProphecyStickModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+                capability.prophecy_stick_mode = finalMode;
+                capability.syncPlayerVariables(entity);
+            });
 
-            String modeName = "";
-			switch ((int) mode) {
-                case 1:
-                    modeName = "coal";
-                    break;
-                case 2:
-                    modeName = "copper";
-                    break;
-                case 4:
-                    modeName = "redstone";
-                    break;
-                case 8:
-                    modeName = "iron";
-                    break;
-                case 16:
-                    modeName = "gold";
-                    break;
-                case 32:
-                    modeName = "diamond";
-                    break;
-                case 64:
-                    modeName = "emerald";
-                    break;
-                default: 
-                    modeName = "The stick is glitching...";
-                    break;
+            String modeName = switch ((int) mode) {
+                case 1 -> "coal";
+                case 2 -> "copper";
+                case 4 -> "redstone";
+                case 8 -> "iron";
+                case 16 -> "gold";
+                case 32 -> "diamond";
+                case 64 -> "emerald";
+                default -> "The stick is glitching...";
+            };
+
+            if (entity instanceof Player _player && !_player.level.isClientSide()) {
+                _player.displayClientMessage(new TextComponent(modeName), (false));
             }
-   
-			if (entity instanceof Player _player && !_player.level.isClientSide()) {
-				_player.displayClientMessage(new TextComponent(modeName), (false));
-			}
-		}
-
-
-
-		}
-	}
+        }
+    }
 }
